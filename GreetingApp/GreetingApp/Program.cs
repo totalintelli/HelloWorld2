2c58fcde268e1e1b1c8b1e61ba70912b514669d2
totalintelli <blueintelli@gmail.com>
2018-04-29 20:47:26 +0900

    변수를 사용하지 않는 새로운 버전을 작성.

--- GreetingApp/GreetingApp/Program.cs
+++ GreetingApp/GreetingApp/Program.cs
@@ -13,18 +13,13 @@ namespace GreetingApp
             // GreeingApp
             // Initialize name to ""
             string name = "";
-            // Initialize greeting to ""
-            string greeting = "";
 
             // Prompt for name with "What is your name?"
             Console.Write("What is your name?");
             name = Console.ReadLine();
 
-            // Convert greeting to "Hello," + name + "nice to meet you!"
-            greeting = "Hello," + name + "nice to meet you!";
-
             // Display "Hello, " + name + "nice to meet you!"
-            Console.WriteLine(greeting);
+            Console.WriteLine("Hello, " + name + ", nice to meet you!");
 
             // End
             Console.ReadLine();
