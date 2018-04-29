c50c0598a0f728d8d30f8f4b7671e2b5c3a09d88
totalintelli <blueintelli@gmail.com>
2018-04-29 20:44:17 +0900

    이름을 입력 받아 이름을 이용하여 인사말을 출력하는 프로그램을 작성.

---
+++ GreetingApp/GreetingApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // GreeingApp
+            // Initialize name to ""
+            string name = "";
+            // Initialize greeting to ""
+            string greeting = "";
+
+            // Prompt for name with "What is your name?"
+            Console.Write("What is your name?");
+            name = Console.ReadLine();
+
+            // Convert greeting to "Hello," + name + "nice to meet you!"
+            greeting = "Hello," + name + "nice to meet you!";
+
+            // Display "Hello, " + name + "nice to meet you!"
+            Console.WriteLine("Hello, " + name + " nice to meet you!");
+
+            // End
+            Console.ReadLine();
+        }
+    }
+}
